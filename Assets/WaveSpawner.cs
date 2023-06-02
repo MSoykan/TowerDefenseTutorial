b2b7f9466bb4687ba28436db011419e3d650f606
MSoykan <mehmetsoykanmutlu@gmail.com>
2023-06-02 17:07:55 +0300

    Added EnemySpawner
    Enemies are spawned from start point by a count down timer. Enemy count is incremented every wave.

---
+++ Assets/WaveSpawner.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 4f;
+    private float countdown = 2f;
+
+    private int waveIndex = 0;
+
+    private void Update()
+    {
+
+        if (countdown <= 0f)
+        {
+           StartCoroutine( SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+    }
+
+    IEnumerator SpawnWave()
+    {
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+        waveIndex++;
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
