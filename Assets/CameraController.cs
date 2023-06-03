def86960d2cbaef21dc50e3f7d4722d9c16508a6
MSoykan <mehmetsoykanmutlu@gmail.com>
2023-06-03 10:22:49 +0300

    Added Zoom in/out to camera
    Camera zooms in with a value among y. Value is clamped  to limit zooming.

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -5,11 +5,25 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
 
+    private bool doMovement = true;
+
     public float panSpeed = 30f;
     public float panBorderThickness = 10f;
 
+    private float scrollSpeed = 10f;
+    public float minY = 10f;
+    public float maxY = 80f;
+
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            doMovement = !doMovement;
+        }
+
+        if (!doMovement)
+            return;
+
         if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
         {
             transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);//is the same as v3(0,0,1)
@@ -26,5 +40,14 @@ public class CameraController : MonoBehaviour
         {
             transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);//is the same as v3(0,0,1)
         }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        Vector3 currentPos = transform.position;
+
+        currentPos.y -= scroll * 1000 *scrollSpeed * Time.deltaTime;
+        currentPos.y = Mathf.Clamp(currentPos.y, minY, maxY);
+
+        transform.position = currentPos;
     }
 }
