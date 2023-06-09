d3204aa1c03e05932aaf342ba5a964976c48f2a1
MSoykan <mehmetsoykanmutlu@gmail.com>
2023-06-09 13:09:49 +0300

    Added HealthBar to enemy prefab
    *Parent Image will be rendered behind child image*
    Basicaly using a canvas and 2 images(with sprite attached to child)

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,30 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
 
     public float startSpeed = 10f;
 
+
+
     [HideInInspector]
     public float speed;
 
+
+    public float startHealth = 100;
     public float health = 100;
 
     public int worth = 50;
 
     public GameObject deathEffect;
 
+    [Header("Unity Stuff")]
+    public Image healthBar;
+
     private void Start()
     {
         speed = startSpeed;
+        health = startHealth;
     }
 
 
     public void TakeDamage(float damageAmount)
     {
         health -= damageAmount;
+
+
+        healthBar.fillAmount = health/startHealth;
 
         if (health <= 0)
         {
